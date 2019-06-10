02e4b43433c5fbdd50d877cbb6d702e48b078785
satorchi <satorchi@apc.in2p3.fr>
2019-06-10 18:06:04 +0200

    better algo with few nested loops

--- qubicpack/plot_fp.py
+++ qubicpack/plot_fp.py
@@ -16,7 +16,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from qubicpack.utilities import NPIXELS, NASIC, ASIC_index, FIGSIZE
-from qubicpack.pix2tes import assign_pix_grid, assign_pix2tes, tes2pix, pix2tes
+from qubicpack.pix2tes import assign_pix_grid, assign_tes_grid
 
 def mylut(v,vmin=3.0,vmax=9.0):
     vfractional=(v-vmin)/(vmax-vmin)
@@ -42,7 +42,6 @@ def plot_fp(args):
       'pngname' : name of file for saving the plot
 
     '''
-    global TES2PIX
     
     # initialize stuff
     pix_grid = assign_pix_grid()
@@ -58,8 +57,8 @@ def plot_fp(args):
         
     obsdate = None
     if 'obsdate' in args.keys():
-        obsdate = args['obsdate']        
-    TES2PIX = assign_pix2tes(obsdate)
+        obsdate = args['obsdate']
+    tes_grid = assign_tes_grid()
 
     if 'pngname' in args.keys():
         pngname = args['pngname']
@@ -88,12 +87,19 @@ def plot_fp(args):
     label_boxprops = dict(boxstyle='round, pad=0.1', facecolor='white', alpha=1.0)
  
     face_colours = {}
+    face_colours['ASIC0'] = 'black'
     face_colours['ASIC1'] = 'white'
     face_colours['ASIC2'] = 'white'
 
     curve_colours = {}
+    curve_colours['ASIC0'] = 'black'
     curve_colours['ASIC1'] = 'black'
     curve_colours['ASIC2'] = 'blue'
+
+    label_colours = {}
+    label_colours['ASIC0'] = 'white'
+    label_colours['ASIC1'] = 'black'
+    label_colours['ASIC2'] = 'blue'
     
     plt.ion()
     fig,ax=plt.subplots(nrows,ncols,figsize=figsize)
@@ -108,55 +114,43 @@ def plot_fp(args):
             ax[row,col].get_yaxis().set_visible(False)
 
             # the pixel identity associated with its physical location in the array
-            physpix=pix_grid[row,col]
-            pix_index=physpix-1
-            face_colour='black'
-            label_colour='white'
-            pix_label='?'
+            TES_parts = np.modf( tes_grid[row,col] )
+            asic_str = '%.0f' % (10*TES_parts[0])
+            TES_str = '%.0f' % TES_parts[1]
+            TES = int(TES_str)
+            TES_idx = TES - 1
+            pix_label = TES_str
+            asic_key = 'ASIC%s' % asic_str
+            asicbg_key = '%s bg' % asic_key
+            asicgood_key = '%s good' % asic_key
+            xaxis_key = '%s x-axis' % asic_key
+            
+            face_colour = face_colours[asic_key]
+            label_colour = label_colours[asic_key]
+            curve_colour = curve_colours[asic_key]
 
             text_y=0.4
             text_x=0.9
             labelfontsize = ttlfontsize
-            if physpix==0:
-                pix_label=''
-                label_colour='black'
-                face_colour='black'
 
-            else:
-                for asic_ctr in range(2):
-                    asic = asic_ctr + 1
-                    asic_idx = ASIC_index(asic)
-                    asic_key = 'ASIC%i' % asic
-                    asicbg_key = 'ASIC%i bg' % asic
-                    asicgood_key = 'ASIC%i good' % asic
 
-                    if asic_key in args.keys() and args[asic_key] is not None:
-                        curve_x = range(args[asic_key].shape[1])
-                    xaxis_key = '%s x-axis' % asic_key
-                    if xaxis_key in args.keys() and args[xaxis_key] is not None:
-                        curve_x = args[xaxis_key]
-                        
-                    if physpix in TES2PIX[asic_idx]:
-                        TES=pix2tes(physpix,asic)
-                        TES_idx = TES - 1
-                        pix_label=str('%i' % TES)
-                        label_colour=curve_colours[asic_key]
-                        face_colour=face_colours[asic_key]
-                        curve_colour=curve_colours[asic_key]
+            if asic_key in args.keys() and args[asic_key] is not None:
+                curve_x = range(args[asic_key].shape[1])
+                curve = args[asic_key][TES_idx]
+                text_x = 0.9
+                text_y = 0.1
+                labelfontsize = fontsize
+                if asicgood_key in args.keys() and not args[asicgood_key][TES_idx]:
+                    face_colour='black'
+                    label_colour='white'
+                    curve_colour='white'
+                elif asicbg_key in args.keys():
+                    face_colour=mylut(args[asicbg_key][TES_idx],lutmin,lutmax)
 
-                        if asic_key in args.keys() and args[asic_key] is not None:
-                            curve = args[asic_key][TES_idx]
-                            text_x = 0.9
-                            text_y = 0.1
-                            labelfontsize = fontsize
-                            if asicgood_key in args.keys() and not args[asicgood_key][TES_idx]:
-                                face_colour='black'
-                                label_colour='white'
-                                curve_colour='white'
-                            elif asicbg_key in args.keys():
-                                face_colour=mylut(args[asicbg_key][TES_idx],lutmin,lutmax)
-                            ax[row,col].plot(curve_x,curve,color=curve_colour)
+            if xaxis_key in args.keys() and args[xaxis_key] is not None:
+                curve_x = args[xaxis_key]
 
+            ax[row,col].plot(curve_x,curve,color=curve_colour)                
             ax[row,col].set_facecolor(face_colour)
             label_boxprops['facecolor'] = face_colour
             ax[row,col].text(text_x,text_y,pix_label,

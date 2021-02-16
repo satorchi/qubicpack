c6133a733d2a441241384ba288da353a756e0a7f
Steve TORCHINSKY <satorchi@apc.in2p3.fr>
2021-02-16 10:21:37 +0100

    cal_sketch:  changes from 2020-07-31 never commited!

--- scripts/cal_sketch.py
+++ scripts/cal_sketch.py
@@ -24,10 +24,10 @@ def cal_sketch(pointing_ang=50,     # QUBIC nominal pointing elevation angle
                cryo_height=1.542618,# height of the QUBIC cryostat
                win_offset=0.241979, # distance of the window from the centre axis of the cryostat
                xlength=13,          # x extent of the drawing
-               ylength=8,           # y extent of the drawing
-               transparency=False,
-               figsize=(16,10)):
-    fontsize=figsize[0]//2
+               ylength=7,           # y extent of the drawing
+               transparency=True,
+               figsize=(20,16),
+               fontsize=16):
     
     window_pos=[-D,Hqubic]
     cal_pos=[0,Hsource]
@@ -36,21 +36,23 @@ def cal_sketch(pointing_ang=50,     # QUBIC nominal pointing elevation angle
     
     fig=plt.figure(figsize=figsize)
     fig.canvas.set_window_title('plt: Calibration Source Sketch')
+    ax = fig.add_axes((0.05,0.05,0.90,0.90))
+    
 
 
     # QUBIC window marker
     x=window_pos[0]
     y=window_pos[1]
-    plt.plot(x,y,color='red',linestyle='none',marker='D')
-    txt='   QUBIC position at (%.2fm, %.2fm)' % (D,Hqubic)
-    plt.text(x,y,txt,color='red',fontsize=fontsize,ha='left',va='center')
+    ax.plot(x,y,color='red',linestyle='none',marker='D')
+    txt='   QUBIC position (%.2fm, %.2fm)' % (D,Hqubic)
+    ax.text(x+0.7,y,txt,color='black',fontsize=fontsize,ha='left',va='bottom')
 
     # cal source position marker
     x=cal_pos[0]
     y=cal_pos[1]
-    plt.plot(x,y,color='green',linestyle='none',marker='D')
-    txt='cal source at height %.2fm  ' % cal_pos[1]
-    plt.text(x,y,txt,color='green',fontsize=fontsize,ha='right',va='bottom')
+    ax.plot(x,y,color='green',linestyle='none',marker='D')
+    txt='cal source height %.2fm  ' % cal_pos[1]
+    ax.text(x,y,txt,color='black',fontsize=fontsize,ha='right',va='bottom')
 
     # line showing QUBIC line of sight
     s=2
@@ -70,12 +72,12 @@ def cal_sketch(pointing_ang=50,     # QUBIC nominal pointing elevation angle
 
     x=[window_pos[0],mirror_pos[0]]
     y=[window_pos[1],mirror_pos[1]]
-    plt.plot(x,y,color='red',linewidth=3)
+    ax.plot(x,y,color='red',linewidth=3)
 
     # line from cal source to mirror
     x=[cal_pos[0],mirror_pos[0]]
     y=[cal_pos[1],mirror_pos[1]]
-    plt.plot(x,y,color='green',linewidth=3)
+    ax.plot(x,y,color='green',linewidth=3)
 
     delta_s=y[0]-y[1]
     delta_c=-(x[1]-x[0])
@@ -99,9 +101,9 @@ def cal_sketch(pointing_ang=50,     # QUBIC nominal pointing elevation angle
     y2=mirror_pos[1]-dy
     x=[mirror_pos[0],x2]
     y=[mirror_pos[1],y2]
-    plt.plot(x,y,color='black',linestyle='dashed',linewidth=1)
+    ax.plot(x,y,color='black',linestyle='dashed',linewidth=1)
     txt='reflection angle at mirror = %.2f$^\circ$' % reflection_ang
-    plt.text(x2,y2,txt,color='black',fontsize=fontsize,ha='left',va='center')
+    ax.text(x2,y2,txt,color='black',fontsize=fontsize,ha='left',va='center')
     
     # a line 1.4m long to show the mirror
     mirror_dx=0.5*mirror_len*cos(mirror_ang_rad)
@@ -109,32 +111,33 @@ def cal_sketch(pointing_ang=50,     # QUBIC nominal pointing elevation angle
 
     x=[mirror_pos[0]+mirror_dx,mirror_pos[0]-mirror_dx]
     y=[mirror_pos[1]-mirror_dy,mirror_pos[1]+mirror_dy]
-    plt.plot(x,y,color='black',linewidth=3)
+    ax.plot(x,y,color='black',linewidth=3)
 
     # a dashed line showing the mirror horizontal extension
     x=[mirror_pos[0]-mirror_dx,mirror_pos[0]+mirror_dx]
     y=[mirror_pos[1]+mirror_dy,mirror_pos[1]+mirror_dy]
-    plt.plot(x,y,color='black',linestyle='dashed')
-    #plt.plot([x[1],x[1]],[y[1],0],color='black',linestyle='dashed')
+    ax.plot(x,y,color='black',linestyle='dashed')
+    #ax.plot([x[1],x[1]],[y[1],0],color='black',linestyle='dashed')
 
     txt='mirror horizontal extension = %.2fm' % abs(2*mirror_dx)
-    plt.text(x[0],y[0],txt,ha='right',va='bottom',fontsize=fontsize,color='black')
+    txt='%.2fm' % abs(2*mirror_dx)
+    ax.text(x[0],y[0],txt,ha='right',va='bottom',fontsize=fontsize,color='black')
     
     if mirror_ang > 90:
         txt='  mirror angle = %.2f$^\circ$' % (mirror_ang-90)
     else:
         txt='  mirror angle = %.2f$^\circ$' % mirror_ang        
-    plt.text(x[0],y[0],txt,ha='left',va='center',fontsize=fontsize,color='black')
+    ax.text(x[0],y[0],txt,ha='left',va='top',fontsize=fontsize,color='black')
 
     # dashed line to show mirror position
     x=[mirror_pos[0],mirror_pos[0]]
     y=[mirror_pos[1],0]
-    plt.plot(x,y,color='black',linestyle='dashed')
+    ax.plot(x,y,color='black',linestyle='dashed')
 
     x=mirror_pos[0]
     y=mirror_pos[1]
-    txt=' flat mirror at (%.2fm, %.2fm)' % (abs(mirror_pos[0]),mirror_pos[1])
-    plt.text(x,y,txt,color='black',ha='right',va='bottom',fontsize=fontsize)
+    txt=' flat mirror at\n%.2fm, %.2fm' % (abs(mirror_pos[0]),mirror_pos[1])
+    ax.text(x,y,txt,color='black',ha='right',va='bottom',fontsize=fontsize)
 
     # total optical path
     Dcal_mirror=sqrt( (cal_pos[0]-mirror_pos[0])**2 + (cal_pos[1]-mirror_pos[1])**2  )
@@ -144,28 +147,25 @@ def cal_sketch(pointing_ang=50,     # QUBIC nominal pointing elevation angle
     # edge line
     x=[0,0]
     y=[0,Hsource]
-    #plt.plot(x,y,color='blue')
+    #ax.plot(x,y,color='blue')
 
     # bottom line
     x=[1-xlength,1]
     y=[0,0]
-    plt.plot(x,y,color='black',linewidth=0.5,linestyle='dotted')
+    ax.plot(x,y,color='black',linewidth=0.5,linestyle='dotted')
 
     # horizontal line at window height to compare to the drawing by Didier
     x=[1-xlength,1]
     y=[window_pos[1],window_pos[1]]
-    plt.plot(x,y,color='black',linewidth=0.5,linestyle='dotted')
+    ax.plot(x,y,color='black',linewidth=0.5,linestyle='dotted')
 
     # draw a box outline of the QUBIC cryostat
     top_centre=[window_pos[0]-win_offset*sin(pointing_ang_rad),window_pos[1]-win_offset*cos(pointing_ang_rad)]
     bot_centre=[top_centre[0]+cryo_height*cos(pointing_ang_rad),top_centre[1]-cryo_height*sin(pointing_ang_rad)]
     x=[bot_centre[0],top_centre[0]]
     y=[bot_centre[1],top_centre[1]]
-    plt.plot(x,y,color='black',linewidth=1,linestyle='dotted')
-    #plt.plot(top_centre[0],top_centre[1],marker='X',color='black')
-    print win_offset
-    print top_centre
-    print bot_centre
+    ax.plot(x,y,color='black',linewidth=1,linestyle='dotted')
+    #ax.plot(top_centre[0],top_centre[1],marker='X',color='black')
 
     cryo_corner1=(top_centre[0]-0.5*cryo_width*sin(pointing_ang_rad),top_centre[1]-0.5*cryo_width*cos(pointing_ang_rad))
     cryo_corner2=(top_centre[0]+0.5*cryo_width*sin(pointing_ang_rad),top_centre[1]+0.5*cryo_width*cos(pointing_ang_rad))
@@ -174,9 +174,8 @@ def cal_sketch(pointing_ang=50,     # QUBIC nominal pointing elevation angle
     pts=[cryo_corner1,cryo_corner2,cryo_corner3,cryo_corner4,cryo_corner1]
     x=[val[0] for val in pts]
     y=[val[1] for val in pts]
-    plt.plot(x,y,color='black',linestyle='dotted')
+    ax.plot(x,y,color='black',linestyle='dotted')
     
-    ax=fig.axes[0]
     # plot extents
     ax.set_xlim(1-xlength,1)
     ax.set_ylim(-1,-1+ylength)
@@ -186,10 +185,11 @@ def cal_sketch(pointing_ang=50,     # QUBIC nominal pointing elevation angle
 
     # labels
     txt='Total optical path length = %.2fm' % optical_dist
-    plt.text(0.5,0.1,txt,va='top',ha='center',fontsize=fontsize,transform=ax.transAxes)
+    ax.text(0.5,0.1,txt,va='top',ha='center',fontsize=fontsize,transform=ax.transAxes)
 
-    plt.title('QUBIC Calibration Source layout')
+    ax.text(0.5,1.01,'QUBIC Calibration Source layout',ha='center',va='bottom',fontsize=fontsize,transform=ax.transAxes)
+    ax.tick_params(labelsize=fontsize)
     
-    pngname='/home/work/qubic/pix/QUBIC_calibration_source_layout_sketch.png'
-    plt.savefig(pngname,format='png',dpi=100,bbox_inches='tight',transparent=True)
+    pngname='/home/work/qubic/pix/calsource_setup/QUBIC_calibration_source_layout_sketch.png'
+    fig.savefig(pngname,format='png',dpi=300,bbox_inches='tight',transparent=transparency)
     return

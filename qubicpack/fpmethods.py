14bead27f8d9c1488cc72db43a0e22767c1b7260
Steve TORCHINSKY <satorchi@apc.in2p3.fr>
2022-09-06 22:21:18 +0200

    bugfix: missing bracket

--- qubicpack/fpmethods.py
+++ qubicpack/fpmethods.py
@@ -211,7 +211,7 @@ def calsource_info(self):
             if val.lower()=='none':
                 info[dev][parm] = 'none'
             else:
-                info[dev][parm] = float(val.replace('%','')
+                info[dev][parm] = float(val.replace('%',''))
             continue
             
 
